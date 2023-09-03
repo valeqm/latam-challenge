e80477b3524da8303466df00ea0dbae9de962ae7
Valeria <valeriaqm_24_11@hotmail.com>
2023-09-03 00:45:05 -0500

    Update model.py

--- challenge/model.py
+++ challenge/model.py
@@ -1,17 +1,18 @@
 import pandas as pd
 from typing import Tuple, Union, List
 from sklearn.model_selection import train_test_split
-from sklearn.utils import shuffle
-from sklearn.metrics import confusion_matrix, classification_report
 import numpy as np
 import xgboost as xgb
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.utils import shuffle
+from datetime import datetime
 
 class DelayModel:
 
     def __init__(
         self
     ):
-        self._model = None # Model should be saved in this attribute.
+        self._model = None  # Model should be saved in this attribute.
 
     def preprocess(
         self,
@@ -30,6 +31,7 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
+
         def get_period_day(date):
             date_time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').time()
             morning_min = datetime.strptime("05:00", '%H:%M').time()
@@ -40,12 +42,12 @@ class DelayModel:
             evening_max = datetime.strptime("23:59", '%H:%M').time()
             night_min = datetime.strptime("00:00", '%H:%M').time()
             night_max = datetime.strptime("4:59", '%H:%M').time()
-
-            if (date_time > morning_min and date_time < morning_max):
+            
+            if(date_time > morning_min and date_time < morning_max):
                 return 'mañana'
-            elif (date_time > afternoon_min and date_time < afternoon_max):
+            elif(date_time > afternoon_min and date_time < afternoon_max):
                 return 'tarde'
-            elif (
+            elif(
                 (date_time > evening_min and date_time < evening_max) or
                 (date_time > night_min and date_time < night_max)
             ):
@@ -54,17 +56,17 @@ class DelayModel:
         def is_high_season(fecha):
             fecha_año = int(fecha.split('-')[0])
             fecha = datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S')
-            range1_min = datetime.strptime('15-Dec', '%d-%b').replace(year=fecha_año)
-            range1_max = datetime.strptime('31-Dec', '%d-%b').replace(year=fecha_año)
-            range2_min = datetime.strptime('1-Jan', '%d-%b').replace(year=fecha_año)
-            range2_max = datetime.strptime('3-Mar', '%d-%b').replace(year=fecha_año)
-            range3_min = datetime.strptime('15-Jul', '%d-%b').replace(year=fecha_año)
-            range3_max = datetime.strptime('31-Jul', '%d-%b').replace(year=fecha_año)
-            range4_min = datetime.strptime('11-Sep', '%d-%b').replace(year=fecha_año)
-            range4_max = datetime.strptime('30-Sep', '%d-%b').replace(year=fecha_año)
-
-            if ((fecha >= range1_min and fecha <= range1_max) or
-                (fecha >= range2_min and fecha <= range2_max) or
+            range1_min = datetime.strptime('15-Dec', '%d-%b').replace(year = fecha_año)
+            range1_max = datetime.strptime('31-Dec', '%d-%b').replace(year = fecha_año)
+            range2_min = datetime.strptime('1-Jan', '%d-%b').replace(year = fecha_año)
+            range2_max = datetime.strptime('3-Mar', '%d-%b').replace(year = fecha_año)
+            range3_min = datetime.strptime('15-Jul', '%d-%b').replace(year = fecha_año)
+            range3_max = datetime.strptime('31-Jul', '%d-%b').replace(year = fecha_año)
+            range4_min = datetime.strptime('11-Sep', '%d-%b').replace(year = fecha_año)
+            range4_max = datetime.strptime('30-Sep', '%d-%b').replace(year = fecha_año)
+            
+            if ((fecha >= range1_min and fecha <= range1_max) or 
+                (fecha >= range2_min and fecha <= range2_max) or 
                 (fecha >= range3_min and fecha <= range3_max) or
                 (fecha >= range4_min and fecha <= range4_max)):
                 return 1
@@ -74,10 +76,10 @@ class DelayModel:
         def get_min_diff(data):
             fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
             fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
-            min_diff = ((fecha_o - fecha_i).total_seconds()) / 60
+            min_diff = ((fecha_o - fecha_i).total_seconds())/60
             return min_diff
 
-        # Aplicar las funciones de preprocesamiento a los datos
+        #preprocessing functions
         data['period_day'] = data['Fecha-I'].apply(get_period_day)
         data['high_season'] = data['Fecha-I'].apply(is_high_season)
         data['min_diff'] = data.apply(get_min_diff, axis=1)
@@ -85,19 +87,38 @@ class DelayModel:
         threshold_in_minutes = 15
         data['delay'] = np.where(data['min_diff'] > threshold_in_minutes, 1, 0)
 
+        #top 10 features
+        top_10_features = [
+            "OPERA_Latin American Wings", 
+            "MES_7",
+            "MES_10",
+            "OPERA_Grupo LATAM",
+            "MES_12",
+            "TIPOVUELO_I",
+            "MES_4",
+            "MES_11",
+            "OPERA_Sky Airline",
+            "OPERA_Copa Air"
+        ]
+
         features = pd.concat([
             pd.get_dummies(data['OPERA'], prefix='OPERA'),
             pd.get_dummies(data['TIPOVUELO'], prefix='TIPOVUELO'),
             pd.get_dummies(data['MES'], prefix='MES')],
             axis=1
         )
-        target = data['delay']
+
+        #consistent set of features
+        features = features[top_10_features]
+
+        target = pd.DataFrame(data['delay'], columns=['delay'])
+
+        self.fit(features, target)
 
         if target_column is not None:
             return features, target
         else:
             return features
-            
 
     def fit(
         self,
@@ -111,12 +132,33 @@ class DelayModel:
             features (pd.DataFrame): preprocessed data.
             target (pd.DataFrame): target.
         """
-        # Entrenar el modelo aquí
-        x_train, x_test, y_train, y_test = train_test_split(features, target, test_size=0.33, random_state=42)
-        self._model = xgb.XGBClassifier(random_state=1, learning_rate=0.01)
-        self._model.fit(x_train, y_train)
-        
-        return
+        #model training code
+        x_train, x_test, y_train, y_test = train_test_split(features, target['delay'], test_size=0.33, random_state=42)
+
+        top_10_features = [
+            "OPERA_Latin American Wings", 
+            "MES_7",
+            "MES_10",
+            "OPERA_Grupo LATAM",
+            "MES_12",
+            "TIPOVUELO_I",
+            "MES_4",
+            "MES_11",
+            "OPERA_Sky Airline",
+            "OPERA_Copa Air"
+        ]
+
+        n_y0 = len(y_train[y_train == 0])
+        n_y1 = len(y_train[y_train == 1])
+        scale = n_y0 / n_y1
+
+        x_train2, x_test2, y_train2, y_test2 = train_test_split(features[top_10_features], target['delay'], test_size=0.33, random_state=42)
+
+        xgb_model_2 = xgb.XGBClassifier(random_state=1, learning_rate=0.01, scale_pos_weight=scale)
+        xgb_model_2.fit(x_train2, y_train2)
+
+        #save the trained model in the class attribute
+        self._model = xgb_model_2
 
     def predict(
         self,
@@ -127,11 +169,25 @@ class DelayModel:
 
         Args:
             features (pd.DataFrame): preprocessed data.
-        
+
         Returns:
             (List[int]): predicted targets.
         """
-        # Realizar predicciones aquí
-        xgboost_y_preds = self._model.predict(features)
-        xgboost_y_preds = [1 if y_pred > 0.5 else 0 for y_pred in xgboost_y_preds]
-        return xgboost_y_preds
+
+        #the top 10 features for prediction
+        top_10_features = [
+            "OPERA_Latin American Wings", 
+            "MES_7",
+            "MES_10",
+            "OPERA_Grupo LATAM",
+            "MES_12",
+            "TIPOVUELO_I",
+            "MES_4",
+            "MES_11",
+            "OPERA_Sky Airline",
+            "OPERA_Copa Air"
+        ]
+
+        #use the trained model to make predictions on the selected features
+        predictions = self._model.predict(features[top_10_features])
+        return predictions.tolist()
